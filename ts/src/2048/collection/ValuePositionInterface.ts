4a4023cd51e9fd913d5186c4e7dd4e825ad14c9b
Joppe Aarts <joppe@apestaartje.info>
2016-06-19 21:41:52 +0200

    ValuePositionInterface added

---
+++ ts/src/2048/collection/ValuePositionInterface.ts
@@ -0,0 +1,18 @@
+/**
+ * @interface ValuePositionInterface
+ */
+interface ValuePositionInterface {
+    /**
+     * The row index
+     *
+     * @type {number}
+     */
+    row:number;
+
+    /**
+     * The column index
+     *
+     * @type {number}
+     */
+    column:number;
+}
