3c8fa848ce3c37db0949d743d9c86270f94a0abf
Joppe Aarts <joppe@apestaartje.info>
2016-11-16 21:25:07 +0100

    fixed collection model getter

--- src/collection/Values.ts
+++ src/collection/Values.ts
@@ -1,5 +1,6 @@
 import * as Backbone from 'backbone';
 import {Value} from './../model/Value';
+import {ValueLiteralInterface} from './../model/ValueLiteralInterface';
 
 /**
  * @class Values
@@ -8,7 +9,7 @@ class Values extends Backbone.Collection<Value> {
     /**
      * @type {Value}
      */
-    get model():{new():Value} {
+    get model():{new(attributes:ValueLiteralInterface):Value} {
         return Value;
     }
 }
