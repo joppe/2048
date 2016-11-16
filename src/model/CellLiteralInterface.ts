2bc54262b70118adf4e28af94765190213748f7e
Joppe Aarts <joppe@zicht.nl>
2016-11-16 13:33:42 +0100

    CellLiteralInterface added

---
+++ src/model/CellLiteralInterface.ts
@@ -0,0 +1,16 @@
+/**
+ * @interface CellLiteralInterface
+ */
+interface CellLiteralInterface {
+    /**
+     * @type {number}
+     */
+    row:number;
+
+    /**
+     * @type {number}
+     */
+    column:number;
+}
+
+export {CellLiteralInterface};
