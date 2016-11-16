b129d9a776f1400f4ff5101e31611aaa010f7070
Joppe Aarts <joppe@zicht.nl>
2016-11-16 13:53:21 +0100

    cell model: getters for row and column added

--- src/model/Cell.ts
+++ src/model/Cell.ts
@@ -6,6 +6,20 @@ import {CellLiteralInterface} from './CellLiteralInterface';
  */
 class Cell extends Backbone.Model {
     /**
+     * @returns {number}
+     */
+    get row():number {
+        return this.get('row');
+    }
+
+    /**
+     * @returns {number}
+     */
+    get column():number {
+        return this.get('column');
+    }
+
+    /**
      * @param {object} attributes
      */
     constructor(attributes:CellLiteralInterface) {
