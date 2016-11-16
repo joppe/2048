1aa2d8524c0db10bd367652754417f846a6df8e4
Joppe Aarts <joppe@zicht.nl>
2016-11-16 13:34:10 +0100

    constructor added to Cell, to enforce attributes type

--- src/model/Cell.ts
+++ src/model/Cell.ts
@@ -1,9 +1,16 @@
 import Backbone from 'backbone';
+import {CellLiteralInterface} from './CellLiteralInterface';
 
 /**
  * @class Cell
  */
 class Cell extends Backbone.Model {
+    /**
+     * @param {object} attributes
+     */
+    constructor(attributes:CellLiteralInterface) {
+        super(attributes);
+    }
 }
 
 export {Cell};
