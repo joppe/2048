1a42b7b6a8f29a689ddd5ed8867a007e7ddf3239
Joppe Aarts <joppe@apestaartje.info>
2016-11-16 21:35:50 +0100

    added vals to GameLiteralInterface

--- src/model/GameLiteralInterface.ts
+++ src/model/GameLiteralInterface.ts
@@ -1,13 +1,21 @@
+import {ValueLiteralInterface} from './ValueLiteralInterface';
 /**
  * @interface GameLiteralInterface
  */
 interface GameLiteralInterface {
     /**
-     * The number of row and columns to use
+     * The number of row and columns to use.
      *
      * @type {number}
      */
     size?:number;
+
+    /**
+     * The values to start with
+     *
+     * @type {object[]}
+     */
+    vals?:ValueLiteralInterface[];
 }
 
 export {GameLiteralInterface};
