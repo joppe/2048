944a242fcd9b0b4c3125c4de90573ef2dbd14b85
Joppe Aarts <joppe@zicht.nl>
2016-11-18 12:20:51 +0100

    Iterators

---
+++ src/iterator/CellIndexIterator.ts
@@ -0,0 +1,85 @@
+import {RangeIterator} from './RangeIterator';
+import {CellIndexInterface} from '../model/CellIndexInterface';
+/**
+ * @class CellIndexIterator
+ */
+class CellIndexIterator implements Iterable<CellIndexInterface> {
+    /**
+     * @type {RangeIterator}
+     */
+    private _innerIterator:RangeIterator;
+
+    /**
+     * @type {RangeIterator}
+     */
+    private _outerIterator:RangeIterator;
+
+    /**
+     * @type {string}
+     */
+    private _innerProperty:string;
+
+    /**
+     * @type {string}
+     */
+    private _outerProperty:string;
+
+    /**
+     * @param {number} size
+     * @param {boolean} isVerticalMovement
+     * @param {boolean} isIncrementalMovement
+     */
+    constructor(size:number, isVerticalMovement:boolean, isIncrementalMovement:boolean) {
+        this._outerProperty = isVerticalMovement ? 'column' : 'row';
+        this._outerIterator = new RangeIterator(
+            0,
+            size - 1,
+            1
+        );
+
+        this._innerProperty = isVerticalMovement ? 'row' : 'column';
+        this._innerIterator = new RangeIterator(
+            isIncrementalMovement ? 0 : size - 1,
+            isIncrementalMovement ? size - 1 : 0,
+            isIncrementalMovement ? 1 : -1
+        );
+    }
+
+    /**
+     * @returns {IteratorResult}
+     */
+    public next():IteratorResult<CellIndexInterface> {
+        let outer:IteratorResult<number> = this._outerIterator.current(),
+            inner:IteratorResult<number> = this._innerIterator.next();
+
+        // If the inner is done, go to the next outer
+        if (true === inner.done) {
+            this._outerIterator.next();
+            outer = this._outerIterator.current();
+            inner = this._innerIterator.next();
+        }
+
+        if (true === outer.done) {
+            return <IteratorResult<CellIndexInterface>>{
+                done: true
+            };
+        } else {
+            return <IteratorResult<CellIndexInterface>>{
+                value: {
+                    [this._outerProperty]: outer.value,
+                    [this._innerProperty]: inner.value
+                } as CellIndexInterface,
+                done: false
+            };
+        }
+    }
+
+    /**
+     * @returns {Range}
+     */
+    [Symbol.iterator]():IterableIterator<CellIndexInterface> {
+        return this;
+    }
+}
+
+export {CellIndexIterator};
--- src/iterator/RangeIterator.ts
+++ src/iterator/RangeIterator.ts
@@ -1,7 +1,7 @@
 /**
- * @class DirectionIterator
+ * @class RangeIterator
  */
-class DirectionIterator implements Iterable<number> {
+class RangeIterator implements Iterable<number> {
     /**
      * @type {number}
      */
@@ -23,35 +23,21 @@ class DirectionIterator implements Iterable<number> {
     private _pointer:number;
 
     /**
-     * @type {string}
-     */
-    private _property:string;
-
-    /**
-     * @returns {string}
-     */
-    get property():string {
-        return this._property;
-    }
-
-    /**
      * @param {number} min
      * @param {number} max
      * @param {number} step
-     * @param {string} property
      */
-    constructor(min:number, max:number, step:number, property:string) {
+    constructor(min:number, max:number, step:number) {
         if (
             (min < max && step <= 0) ||
             (min > max && step >= 0)
         ) {
-            throw new Error(`Argument error, min: ${min} max: ${max} step: ${step}`);
+            throw new Error(`RangeIterator.constructor: error, min: ${min} max: ${max} step: ${step}`);
         }
 
         this._min = min;
         this._max = max;
         this._step = step;
-        this._property = property;
         this._pointer = 0;
     }
 
@@ -59,26 +45,49 @@ class DirectionIterator implements Iterable<number> {
      * @returns {IteratorResult}
      */
     public next():IteratorResult<number> {
+        let result:IteratorResult<number> = this.current();
+
+        if (true === result.done) {
+            this._pointer = 0;
+        } else {
+            this._pointer += 1;
+        }
+
+        return result;
+    }
+
+    /**
+     * @returns {IteratorResult}
+     */
+    public current():IteratorResult<number> {
         let value:number = this._min + (this._step * this._pointer);
 
         if (
             (this._max > this._min && value <= this._max) ||
             (this._min > this._max && value >= this._max)
         ) {
-            this._pointer += 1;
-
             return <IteratorResult<number>>{
                 done: false,
                 value
             };
         } else {
-            this._pointer = 0;
-
             return <IteratorResult<number>>{
                 done: true
             };
         }
     }
+
+    /**
+     * @returns {IteratorResult}
+     *
+    public return():IteratorResult<number> {
+        this._pointer = 0;
+
+        return {
+            done: true
+        };
+    }
+    /**/
 
     /**
      * @returns {Range}
@@ -88,4 +97,4 @@ class DirectionIterator implements Iterable<number> {
     }
 }
 
-export {DirectionIterator};
+export {RangeIterator};
